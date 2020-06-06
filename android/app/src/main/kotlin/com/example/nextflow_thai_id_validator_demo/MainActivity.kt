273bbd49300cb2e95bc23be0cde5906a81a9179c
Teerasej <teerasej@gmail.com>
2020-06-06 17:33:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/nextflow_thai_id_validator_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_thai_id_validator_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:nextflow_thai_personal_id/nextflow_thai_personal_id.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Flutter Widget Today',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter 3 นาที: Id เธอนั้นถูกต้องไหม'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ThaiIdValidator validator =
+      ThaiIdValidator();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Builder(
+          builder: (context) => Padding(
+            padding: EdgeInsets.all(10),
+            child: Form(
+                key: _formKey,
+                child: Column(children: <Widget>[
+                  TextFormField(
+                    validator: validator.validate,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text('ถูกต้องแล้วจ้า')));
+                      }
+                    },
+                    child: Text('Check'),
+                  )
+                ])),
+          ),
+        ));
+  }
+}
